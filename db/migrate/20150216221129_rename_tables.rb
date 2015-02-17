faa16d4b3f2b8d724dc9f8271c3e9bfbd15d4c0f
Raekye <adrian@creatifcubed.com>
2015-02-17 00:17:31 -0500

    - fix migration swapping tables - fix display bug

--- db/migrate/20150216221129_rename_tables.rb
+++ db/migrate/20150216221129_rename_tables.rb
@@ -1,30 +1,38 @@
 class RenameTables < ActiveRecord::Migration
   def change
+    reversible do |change|
+      change.up do
+        remove_foreign_key :minecrafts, :users
+        remove_foreign_key :servers, :minecrafts
+        remove_foreign_key :server_logs, :minecrafts
+        remove_foreign_key :minecrafts_users, :minecrafts
+        remove_foreign_key :minecrafts_users, :users
+      end
+      change.down do
+        add_foreign_key :minecrafts, :users, { on_delete: :cascade }
+        add_foreign_key :servers, :minecrafts, { on_delete: :cascade }
+        add_foreign_key :server_logs, :minecrafts, { on_delete: :cascade }
+        add_foreign_key :minecrafts_users, :minecrafts, { on_delete: :cascade }
+        add_foreign_key :minecrafts_users, :users, { on_delete: :cascade }
+      end
+    end
+
     rename_table :minecrafts, :servers2
+    rename_index :servers2, :minecrafts_pkey, :servers2_pkey
     rename_table :servers, :minecrafts
     rename_table :servers2, :servers
+    rename_index :servers, :servers2_pkey, :servers_pkey
+
     rename_table :minecrafts_users, :servers_users
 
     rename_column :minecrafts, :minecraft_id, :server_id
     rename_column :servers_users, :minecraft_id, :server_id
     rename_column :server_logs, :minecraft_id, :server_id
+
+    add_foreign_key :servers, :users, { on_delete: :cascade }
+    add_foreign_key :minecrafts, :servers, { on_delete: :cascade }
+    add_foreign_key :server_logs, :servers, { on_delete: :cascade }
+    add_foreign_key :servers_users, :servers, { on_delete: :cascade }
+    add_foreign_key :servers_users, :users, { on_delete: :cascade }
   end
-=begin
-  def up
-    return
-    remove_foreign_key :minecrafts, :users
-    remove_foreign_key :minecrafts_users, :minecrafts
-    remove_foreign_key :minecrafts_users, :sers
-    remove_foreign_key :server_logs, :minecrafts
-    remove_foreign_key :servers, :minecrafts
-  end
-  def down
-    return
-    add_foreign_key :minecrafts, :users, { on_delete: :cascade }
-    add_foreign_key :minecrafts_users, :minecrafts, { on_delete: :cascade }
-    add_foreign_key :minecrafts_users, :users, { on_delete: :cascade }
-    add_foreign_key :server_logs, :minecrafts, { on_delete: :cascade }
-    add_foreign_key :servers, :minecrafts, { on_delete: :cascade }
-  end
-=end
 end
