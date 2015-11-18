39a6319a7239fc970d522c8f9725bf97a8f0f67a
Raekye <adrian@creatifcubed.com>
2015-11-18 04:39:34 -0500

    update sidekiq 4, don't use namespace

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,10 @@
 Sidekiq.configure_client do |config|
-  config.redis = { namespace: "gamocosm-#{Rails.env}" }
+  # see
+  # - http://www.mikeperham.com/2015/09/24/storing-data-with-redis/
+  # - https://github.com/mperham/sidekiq/blob/master/lib/sidekiq/redis_connection.rb
+  # - https://github.com/redis/redis-rb
+  config.redis = { db: (Rails.env.production? ? 0 : (Rails.env.development? ? 1 : 2)) }
 end
 Sidekiq.configure_server do |config|
-  config.redis = { namespace: "gamocosm-#{Rails.env}" }
+  config.redis = { db: (Rails.env.production? ? 0 : (Rails.env.development? ? 1 : 2)) }
 end
