95b8949bee85faca93abaa759e3bf8d5d71a342f
Jono Mingard <reason.koan@gmail.com>
2016-11-03 23:30:12 +1300

    More improvements to control panel strip
    Make it look good in all cases rather than just when the server is off

--- app/helpers/servers_helper.rb
+++ app/helpers/servers_helper.rb
@@ -1,2 +1,21 @@
 module ServersHelper
+  def server_status_class(server)
+    if server.pending_operation
+      'text-warning'
+    elsif server.remote.error?
+      'text-danger'
+    elsif server.running?
+      'text-success'
+    else
+      'text-muted'
+    end
+  end
+
+  def server_minecraft_status_class(server)
+    if server.minecraft.pause?.nil?
+      'text-success'
+    else
+      'text-muted'
+    end
+  end
 end
