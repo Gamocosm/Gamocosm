393ec7601ca3b3920c3e775e45e75b2536ca0d76
Raekye <Raekye@users.noreply.github.com>
2020-04-08 04:05:51 -0400

    change sidekiq redis db

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,9 +3,9 @@ Sidekiq.configure_client do |config|
   # - http://www.mikeperham.com/2015/09/24/storing-data-with-redis/
   # - https://github.com/mperham/sidekiq/blob/master/lib/sidekiq/redis_connection.rb
   # - https://github.com/redis/redis-rb
-  config.redis = { db: (Rails.env.production? ? 12 : (Rails.env.development? ? 1 : 2)) }
+  config.redis = { db: (Rails.env.production? ? 4 : (Rails.env.development? ? 1 : 2)) }
 end
 Sidekiq.configure_server do |config|
-  config.redis = { db: (Rails.env.production? ? 12 : (Rails.env.development? ? 1 : 2)) }
+  config.redis = { db: (Rails.env.production? ? 4 : (Rails.env.development? ? 1 : 2)) }
   Rails.logger = Sidekiq.logger
 end
