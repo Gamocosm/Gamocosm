78e046aea98c215984eb1b1e196128e97e126361
Raekye <Raekye@users.noreply.github.com>
2023-07-23 17:44:11 -0400

    rubocop --autocorrect --only Layout/ArgumentAlignment

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable
 
   has_many :servers, dependent: :destroy
   has_many :volumes, dependent: :destroy
